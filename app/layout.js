5656bd92f7c403d9a907f7012028ddcf3f05b3e9
Karthik Namboori <103726023+karthik2603-theBrogrammer@users.noreply.github.com>
2023-07-15 11:43:47 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,12 +1,10 @@
 "use client";
 import { useEffect } from "react";
 import "./globals.css";
-import { Inter } from "next/font/google";
 import AOS from "aos";
 import "aos/dist/aos.css";
 import { Analytics } from "@vercel/analytics/react";
 
-const inter = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({ children }) {
   useEffect(() => {
@@ -18,7 +16,7 @@ export default function RootLayout({ children }) {
   }, []);
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body>
         {children} 
         <Analytics />
       </body>
