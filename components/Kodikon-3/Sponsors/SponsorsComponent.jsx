257054dc9972ae02864c833d7462309cf9638cae
Karthik Namboori <103726023+karthik2603-theBrogrammer@users.noreply.github.com>
2023-10-15 23:53:08 +0530

    move to pred

--- components/Kodikon-3/Sponsors/SponsorsComponent.jsx
+++ components/Kodikon-3/Sponsors/SponsorsComponent.jsx
@@ -4,11 +4,11 @@ import TitleComponent from '../HelperComponents/TitleComponent'
 import { sponsors } from '@/constants'
 const SponsorCard = ({ sponsorName, sponsorImageURL, sponsorWebsiteURL, index }) => {
   return (
-    <div className="flex flex-col items-center justify-center p-2  h-100 w-100 rounded-lg hover:scale-105 transition ease-out duration-250   "
+    <div className="flex flex-col items-center justify-center p-2  h-100 w-100     "
     data-aos="zoom-in-up"
         data-aos-duration="500"
         data-aos-delay={`${(index + 1) * 200}`}>
-      <Image src={sponsorImageURL} alt={sponsorName} height={500} width={500}  />
+      <Image src={sponsorImageURL} alt={sponsorName} height={500} width={500} className = 'rounded-md' />
     </div>
   )
 }
