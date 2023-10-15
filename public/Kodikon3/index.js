39aab93c72c85fde919f074d4c58feee4cc2c714
rahul <rahul.baradol.14@gmail.com>
2023-10-15 16:00:19 +0530

    Hero Section

---
+++ public/Kodikon3/index.js
@@ -0,0 +1,17 @@
+import whitepeslogo from './whitepeslogo.png'
+import whitelogo from './whitelogo.png'
+import copyofcopy from './copyofcopy.png'
+import leftguy from './leftguy.png'
+import rightguy from './rightguy.png'
+import fourteen from './14.png'
+import fifteen from './15.png'
+
+export {
+    whitepeslogo,
+    whitelogo,
+    copyofcopy,
+    leftguy,
+    rightguy,
+    fourteen,
+    fifteen
+};
