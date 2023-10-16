0c76558928f503498ccdd9f7e82e034d4cc7626d
Karthik Namboori <103726023+karthik2603-theBrogrammer@users.noreply.github.com>
2023-10-16 16:18:20 +0530

    timer-fix

--- components/Kodikon-3/Countdown/EventCountdown.jsx
+++ components/Kodikon-3/Countdown/EventCountdown.jsx
@@ -7,43 +7,53 @@ import Countdown from 'react-countdown';
 const renderer = ({ days, hours, minutes, seconds, completed }) => {
     if (completed) {
         return <div className="h-fit my-32 px-2"><h1 className="text-white text-5xl md:text-6xl lg:text-8xl text-center"><span className="gradient-text-animation">Kodikon 3.0</span> Registration Open!</h1>
-        <div className="w-full h-fit flex items-center justify-center">
+            <div className="w-full h-fit flex items-center justify-center">
                 <button className="text-black text-xl md:text-3xl  text-center bg-white p-3 rounded-md my-12 hover:bg-gray-300 transition ease-in duration-200">Register!</button>
             </div></div>;
     } else {
         return <div className="h-fit my-32 px-2 flex flex-col items-center justify-center gap-y-8">
             <h1 className="text-white text-5xl md:text-6xl lg:text-8xl text-center">Registrations Open In</h1>
             <div className="flex gap-3 text-gray-200 md:text-5xl">
-            <div><span className="text-4xl md:text-6xl lg:text-7xl text-center bg-clip-text">{days}</span>days</div> 
-            <div><span className="text-4xl md:text-6xl lg:text-7xl text-center bg-clip-text">{hours}</span>hours</div> 
-            <div><span className="text-4xl md:text-6xl lg:text-7xl text-center bg-clip-text">{minutes}</span>min</div> 
-            <div><span className="text-4xl md:text-6xl lg:text-7xl text-center bg-clip-text">{seconds}</span>sec</div>
+                <div><span className="text-4xl md:text-6xl lg:text-7xl text-center bg-clip-text">{days}</span>days</div>
+                <div><span className="text-4xl md:text-6xl lg:text-7xl text-center bg-clip-text">{hours}</span>hours</div>
+                <div><span className="text-4xl md:text-6xl lg:text-7xl text-center bg-clip-text">{minutes}</span>min</div>
+                <div><span className="text-4xl md:text-6xl lg:text-7xl text-center bg-clip-text">{seconds}</span>sec</div>
             </div>
-{/* <h1 className=" text-4xl md:text-6xl lg:text-8xl text-center bg-clip-text text-gray-200">{days}d:{hours}h:{minutes}min:{seconds}s</h1> */}
+            {/* <h1 className=" text-4xl md:text-6xl lg:text-8xl text-center bg-clip-text text-gray-200">{days}d:{hours}h:{minutes}min:{seconds}s</h1> */}
         </div>;
     }
 };
 
 const EventCountdown = () => {
     const [timeLeft, setTimeLeft] = useState(null)
+    const date = new Date("2023-10-16T21:00:00");
+    let registrationsBeginDate = date.getTime()
     useEffect(() => {
         axios.get('/api/getTime').then((res) => {
             setTimeLeft(res.data.registrationsBeginDate)
         })
     })
     return (
+        // <div className="h-fit md:py-5">
+        //     {
+        //         timeLeft === null ? (
+        //             <h1 className="text-white text-5xl md:text-6xl lg:text-8xl text-center">Countdown Coming Soon!</h1>
+        //         ) : (
+        //             <Countdown
+        //                 date={ registrationsBeginDate = date.getTime()
+        //                 }
+        //                 renderer={renderer}
+        //             />
+        //         )
+        //     }
+        // </div>
         <div className="h-fit md:py-5">
-            {
-                timeLeft === null ? (
-                    <h1 className="text-white text-5xl md:text-6xl lg:text-8xl text-center">Countdown Coming Soon!</h1>
-                ) : (
-                    <Countdown
-                        date={timeLeft}
-                        renderer={renderer}
-                    />
-                )
-            }
+            <Countdown
+                date={registrationsBeginDate = date.getTime()}
+                renderer={renderer}
+            />
         </div>
+
     )
 }
 export default EventCountdown
--- pages/api/getTime.js
+++ pages/api/getTime.js
@@ -1,8 +1,8 @@
 export default async function getTime(req, res) {
   const date = new Date("2023-10-16T21:00:00");
-  const isoDate = date.toISOString();
   res.status(200).json({
-    currentTime: new Date().toISOString(),
-    registrationsBeginDate: isoDate,
+    currentTime: Date.now(),
+    registrationsBeginDate: date.getTime(),
   });
 }
+
