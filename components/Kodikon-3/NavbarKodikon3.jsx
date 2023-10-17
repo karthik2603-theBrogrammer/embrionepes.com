27daca64bf053c1639af1a0286d05ca6bf9f8ca1
shivangjhalani <shivang2004jhalani@gmail.com>
2023-10-17 07:17:04 +0530

    Close navmenu automatically on small devices

--- components/Kodikon-3/NavbarKodikon3.jsx
+++ components/Kodikon-3/NavbarKodikon3.jsx
@@ -98,19 +98,19 @@ export const NavbarKodikon3 = () => {
                     >
                         <ul className="items-center justify-center space-y-8 md:flex md:space-x-3 lg:space-x-6 md:space-y-0">
                             <li className="text-white text-xs lg:text-[16px]">
-                                <Link href="/kodikon-3/#about">About</Link>
+                                <Link href="/kodikon-3/#about" onClick={() => setNavbar(false)}>About</Link>
                             </li>
                             <li className="text-white text-xs lg:text-[16px]">
-                                <Link href="/kodikon-3/#theme">Themes</Link>
+                                <Link href="/kodikon-3/#theme" onClick={() => setNavbar(false)}>Themes</Link>
                             </li>
                             <li className="text-white text-xs lg:text-[16px]">
-                                <Link href="/kodikon-3/#timeline">Timeline</Link>
+                                <Link href="/kodikon-3/#timeline" onClick={() => setNavbar(false)}>Timeline</Link>
                             </li>
                             <li className="text-white text-xs lg:text-[16px]">
-                                <Link href="/kodikon-3/#sponser">Sponsers</Link>
+                                <Link href="/kodikon-3/#sponser" onClick={() => setNavbar(false)}>Sponsers</Link>
                             </li>
                             <li className="text-white text-xs lg:text-[16px]">
-                                <Link href="/kodikon-3/#faq">FAQ</Link>
+                                <Link href="/kodikon-3/#faq" onClick={() => setNavbar(false)}>FAQ</Link>
                             </li>
                             <li className="text-white text-xs lg:text-[16px]">
                                 <Link href="/#contact">Contact Us</Link>
