69233fbd37ea2e20df61e9e7ae798d9f7dc9996e
Karthik Namboori <103726023+karthik2603-theBrogrammer@users.noreply.github.com>
2023-10-20 17:36:24 +0530

    prizes

--- components/Kodikon-3/Prizes/PrizesComponent.jsx
+++ components/Kodikon-3/Prizes/PrizesComponent.jsx
@@ -3,15 +3,15 @@ import TitleComponent from '../HelperComponents/TitleComponent'
 import { sponsors } from '@/constants'
 import Image from 'next/image'
 
-const PrizeCard = ({ source, index}) => {
+const PrizeCard = ({ source, index }) => {
   return (
     <div className=" h-[200px] w-[200px]"
-    data-aos="zoom-in-up"
-        data-aos-duration="1500"
-        // data-aos-delay={`${(index + 1) * 100}`}
-        >
-          <Image src={source} alt = {`${index + 1}`} height={800} width={800} className = ' bg-transparent'/>
-        </div>
+      data-aos="zoom-in"
+      data-aos-duration="1500"
+    // data-aos-delay={`${(index + 1) * 100}`}
+    >
+      <Image src={source} alt={`${index + 1}`} height={800} width={800} className=' bg-transparent' />
+    </div>
   )
 }
 
@@ -21,9 +21,9 @@ const PrizesComponent = () => {
     <div className="flex flex-col w-full h-fit gap-6">
       <TitleComponent titleData="Prizes in Store!" />
       <div className="w-full h-fit flex flex-col md:grid md:grid-cols-3  place-items-center my-16 gap-9 md:gap-0 ">
-          <PrizeCard index={1}  source = {'/Kodikon3/prizes/first.png'} />
-          <PrizeCard index={2}  source = {'/Kodikon3/prizes/second.png'}/>
-          <PrizeCard index ={3} source = {'/Kodikon3/prizes/third.png'}/>
+        <PrizeCard index={1} source={'/Kodikon3/prizes/first.png'} />
+        <PrizeCard index={2} source={'/Kodikon3/prizes/second.png'} />
+        <PrizeCard index={3} source={'/Kodikon3/prizes/third.png'} />
       </div>
 
     </div>
